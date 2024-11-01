7cf503536c9e9e1fc4c205d88cc203b100827f8d
devowl04 <devowl.04@gmail.com>
2024-11-01 17:05:48 +0530

    demo2

--- src/Components/form.tsx
+++ src/Components/form.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    subscriptionFre: '',
+    option: ''
+  });
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (change: { name: string; value: any }) => {
+    setState((p) => ({
+      ...p,
+      [change.name]: change.value,
+    }));
+  };
+
   return (
     <div>
-      Form
+      <h1>Form </h1>
+      <form name='form' onSubmit={handleSubmit}>
+        <div>
+          Name:
+          <input
+            type='text'
+            name='name'
+            id='name'
+            value={state.name}
+            onChange={(e) => handleChange({ name: 'name', value: e.target.value })}
+          />
+        </div>
+        <div>
+          Email:
+          <input
+            type='email'
+            name='email'
+            id='email'
+            value={state.email}
+            onChange={(e) => handleChange({ name: 'email', value: e.target.value })}
+          />
+        </div>
+        <div>
+          Subscription Fre:
+          <input
+            type='text'
+            name='subscriptionFre'
+            id='subscriptionFre'
+            value={state.subscriptionFre}
+            onChange={(e) => handleChange({ name: 'subscriptionFre', value: e.target.value })}
+          />
+        </div>
+        <div>
+          Option:
+          <input
+            type='text'
+            name='option'
+            id='option'
+            value={state.option}
+            onChange={(e) => handleChange({ name: 'option', value: e.target.value })}
+          />
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
